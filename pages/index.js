673e95f9ef9dc1fff44e3e7fcaf390243acbb37b
scoutbotai <scoutbotai@gmail.com>
2025-04-04 08:42:13 -0700

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import InputForm from '../components/InputForm'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-orange-50 text-gray-900 p-8">
+      <h1 className="text-3xl font-bold mb-6 text-orange-600">ScoutBot</h1>
+      <InputForm />
+    </main>
+  )
+}
